101297d10dd7ecca53e2619ad3551a74e7660d64
linyouwei <yjlyw020150@163.com>
2018-08-14 00:51:47 +0800

    请求数据

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import api from './api/index.js'
+// 将API方法绑定到全局
+Vue.prototype.$api = api
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
